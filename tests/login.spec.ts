609d1e60895f5c79021c1c49e80769ced193d5ea
Chelsey Rochester <cjankord43@yahoo.com>
2024-11-30 09:45:29 -0600

    basic login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from './fixtures/base';
 import { LoginPage } from './pageObjects/pom';
+import { page } from '@playwright/test'
 
 const {user, password} = process.env;
 
 test("log into automationexercise", async ({
+    page,
     loginPage,
 }) => {
     await loginPage.goto();
     await loginPage.login(user, password);
+
+    const logoutButton = page.locator('a[href="/logout"]');
+    await expect(logoutButton).toBeVisible();
 
 });
